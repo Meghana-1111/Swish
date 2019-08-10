996862466b225d615e1b2865061c62676ccd9469
Meghana-1111 <53974291+Meghana-1111@users.noreply.github.com>
2019-08-10 20:03:42 +0530

    Coffee.java

--- Coffee.java
+++ Coffee.java
@@ -6,10 +6,11 @@ public static void main(String args[])
 Scanner sc=new Scanner(System.in);
 int x=sc.nextInt();
 int p=sc.nextInt();
+  int v;
 while(x)
 {
-p=x-90;
-x=p;
+v=x-p;
+x=v;
 if(x==0)
 {
 System.out.print("Free"); 
